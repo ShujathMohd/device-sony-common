e6f4e7541d1fd6ee321eec9d64bec6fc6bcac5cf
Pavel Dubrova <pashadubrova@gmail.com>
2025-06-14 09:20:46 +0530

    common-treble: Switch to Vibrator HAL to AIDL The QTI Vibrator AIDL HAL can handle all hardware from Seine to Yodo.
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- CommonConfig.mk
+++ CommonConfig.mk
@@ -217,12 +217,6 @@ else
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/vintf/android.hw.keymaster_v3.xml
 endif
 
-ifeq ($(TARGET_VIBRATOR_V1_2),true)
-DEVICE_MANIFEST_FILE += $(COMMON_PATH)/vintf/android.hardware.vibrator_v1.2.xml
-else
-DEVICE_MANIFEST_FILE += $(COMMON_PATH)/vintf/android.hardware.vibrator_v1.0.xml
-endif
-
 # CamX manifests
 ifneq ($(filter edo,$(SOMC_PLATFORM)),)
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/vintf/vendor.qti.hardware.camera.postproc.xml
